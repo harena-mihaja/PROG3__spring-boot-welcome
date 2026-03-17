eece6880f105cd6020916092b744b46e311af184
harena-mihaja <mihaja2506@gmail.com>
2026-03-17 16:29:57 +0300

    chore: initial commit

---
+++ src/main/java/edu/school/hei/prog3__springtd2/Prog3SpringTd2Application.java
@@ -0,0 +1,13 @@
+package edu.school.hei.prog3__springtd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3SpringTd2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prog3SpringTd2Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/school/hei/prog3__springtd2/Prog3SpringTd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.school.hei.prog3__springtd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3SpringTd2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
