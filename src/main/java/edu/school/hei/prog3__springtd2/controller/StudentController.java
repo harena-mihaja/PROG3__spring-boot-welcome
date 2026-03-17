3a0cd08e2b5da80b73a811e63029bfce97796776
harena-mihaja <mihaja2506@gmail.com>
2026-03-17 19:18:21 +0300

    feat: implement createStudents in StudentController

--- src/main/java/edu/school/hei/prog3__springtd2/controller/StudentController.java
+++ src/main/java/edu/school/hei/prog3__springtd2/controller/StudentController.java
@@ -1,6 +1,7 @@
 package edu.school.hei.prog3__springtd2.controller;
 
 import edu.school.hei.prog3__springtd2.entity.Student;
+import edu.school.hei.prog3__springtd2.service.StudentService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,15 @@ import java.util.List;
 @RestController
 public class StudentController {
 
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
     @PostMapping("/students")
-    public List<Student> createStudents(@RequestBody List<Student> newStudents)
+    public List<Student> createStudents(@RequestBody List<Student> newStudentsList)
     {
-        throw new UnsupportedOperationException("Not implemented");
+        return (service.createStudents(newStudentsList));
     }
 }
