b66a7c9c28acbb6e8bd9945708b61ce93859f77e
harena-mihaja <mihaja2506@gmail.com>
2026-03-17 18:54:23 +0300

    feat: add method to create students

---
+++ src/main/java/edu/school/hei/prog3__springtd2/service/StudentService.java
@@ -0,0 +1,15 @@
+package edu.school.hei.prog3__springtd2.service;
+
+import edu.school.hei.prog3__springtd2.entity.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> createStudents(List<Student> newStudents)
+    {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
