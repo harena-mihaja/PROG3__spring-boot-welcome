a02def3597137b06ff8c2b553c74d53e063a7dcd
harena-mihaja <mihaja2506@gmail.com>
2026-03-17 19:06:23 +0300

    feat: add save method to the repository

---
+++ src/main/java/edu/school/hei/prog3__springtd2/repository/StudentRepository.java
@@ -0,0 +1,20 @@
+package edu.school.hei.prog3__springtd2.repository;
+
+import edu.school.hei.prog3__springtd2.entity.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class StudentRepository {
+    private final List<Student> studentList;
+
+    public StudentRepository(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public List<Student> saveStudents(List<Student> newStudentsList) {
+        studentList.addAll(newStudentsList);
+        return (newStudentsList);
+    }
+}
