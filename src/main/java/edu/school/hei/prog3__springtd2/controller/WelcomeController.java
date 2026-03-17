6d423982ceb0b1bcab6432b67e9bce65d4360357
harena-mihaja <mihaja2506@gmail.com>
2026-03-17 16:40:13 +0300

    feat: add GET welcome + name

---
+++ src/main/java/edu/school/hei/prog3__springtd2/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package edu.school.hei.prog3__springtd2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping(value = "/welcome")
+    public String welcome(@RequestParam(defaultValue = "User") String name){
+        String template = "Welcome %s";
+        return (template.formatted(name));
+    }
+}
