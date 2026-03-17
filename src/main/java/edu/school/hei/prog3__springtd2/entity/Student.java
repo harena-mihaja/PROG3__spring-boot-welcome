6bd2c9bbaccac0110cfb395fdd6719a0d99cac6a
harena-mihaja <mihaja2506@gmail.com>
2026-03-17 16:46:46 +0300

    feat: add entity Student

--- src/main/java/edu/school/hei/prog3__springtd2/entity/Student.java
+++ src/main/java/edu/school/hei/prog3__springtd2/entity/Student.java
@@ -1,4 +1,15 @@
 package edu.school.hei.prog3__springtd2.entity;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@EqualsAndHashCode
+@ToString
 public class Student {
+    private String reference;
+    private String fristName;
+    private String lastName;
+    private Integer age;
 }
